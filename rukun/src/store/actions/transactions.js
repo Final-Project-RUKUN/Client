43e4e2fb02da7198093144aab3b6859c15553638
gondhes <fajarwirazdi@gmail.com>
2021-04-17 10:35:36 +0700

    fetch users berhasil

---
+++ rukun/src/store/actions/transactions.js
@@ -0,0 +1,16 @@
+export function setTransactions(payload) {
+    return { type: 'transactions/setTransactions', payload }
+  }
+  
+  export function setTransactionsAsync() {
+    const url = 'http://localhost:3001/transactions'
+  
+    return (dispatch) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => {
+          dispatch(setTransactions(data))
+        })
+        .catch(err => console.log(err))
+    }
+  }
---
+++ rukun/src/store/actions/users.js
@@ -0,0 +1,16 @@
+export function setUsers(payload) {
+  return { type: 'users/setUsers', payload }
+}
+
+export function setUsersAsync() {
+  const url = 'http://localhost:3002/users'
+
+  return (dispatch) => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        dispatch(setUsers(data))
+      })
+      .catch(err => console.log(err))
+  }
+}
