ea566f4317691c578668b77b1bde952edb4182e2
gondhes <fajarwirazdi@gmail.com>
2021-04-19 23:12:32 +0700

    fetch account card

--- rukun/src/store/actions/users.js
+++ rukun/src/store/actions/users.js
@@ -4,6 +4,10 @@ export function setUsers(payload) {
   return { type: 'users/setUsers', payload }
 }
 
+export function setAdmin(payload) {
+  return { type: 'admin/setAdmin', payload }
+}
+
 export function setUsersAsync() {
   const url = 'http://localhost:4000/villagers'
 
@@ -17,6 +21,24 @@ export function setUsersAsync() {
     })
       .then(({data}) => {
         dispatch(setUsers(data.Users))
+      })
+      .catch(err => console.log(err))
+  }
+}
+
+export function getAdmin() {
+  const url = 'http://localhost:4000/user/'
+
+  return (dispatch) => {
+    axios({
+      method: 'GET',
+      url,
+      headers: {
+        access_token: localStorage.access_token
+      }
+    })
+      .then(({data}) => {
+        dispatch(setAdmin(data))
       })
       .catch(err => console.log(err))
   }
